60c7fa3b325922a7ac1f42d8e01f801231a55043
Andrey Eremeev <Axtone4all@yandex.ru>
2019-04-26 09:32:12 +0300

    super comb reduction

--- SuperCombinator.scala
+++ SuperCombinator.scala
@@ -3,15 +3,26 @@ package lambda
 trait SPTerm
 
 case class SPVar(name: String) extends SPTerm {
-    override def toString():String = name.toString
+    override def toString: String = name.toString
 }
 
-case class SPDef(vars: Array[SPVar], body: SPTerm) extends SPTerm {
-    override def toString():String = "λ" + vars.map(_.toString()).foldLeft("")(_ + " " + _) + "." + body.toString()
+case class SPDef(vars: List[SPVar], body: SPTerm) extends Combinator {
+  override def toString: String = "[" + vars.mkString(" ") + "]" + body
+  override def result(x: List[SPTerm]): Option[SPTerm] = if (x.length == vars.length) {
+    val ctn = (vars zip x).toMap
+    lazy val substitution:SPTerm => SPTerm = {
+      case v: SPVar => ctn get v getOrElse v
+      case SPAppl(t1, t2) => SPAppl(substitution(t1), substitution(t2))
+      case default => default
+    }
+    Some(substitution(body))
+  } else {
+    None
+  }
 }
 
-case class SPAppl(func: SPTerm, args: Array[SPTerm]) extends SPTerm {
-    override def toString():String = "(" + func.toString() + " " + args.map(_ toString()).foldLeft("")(_ + " " + _) + ")"
+case class SPAppl(term1: SPTerm, term2: SPTerm) extends SPTerm {
+    override def toString: String = s"($term1 $term2)"
 }
 
 
@@ -27,15 +38,15 @@ object SuperCombinator {
           if (fromLambda) {
             lambdaLifting(body, boundVariables + (variable), freeVariables, true)
           } else {
-            lambdaLifting(body, Set(variable), freeVariables ++ boundVariables, true)
+            lambdaLifting(body, Set(variable), freeVariables, true)
           }
         val func: SPDef = newSP match {
-          case SPDef(vars, body) => SPDef(SPVar(variable name) +: vars, body)
-          case default => SPDef(Array(SPVar(variable.name)), newSP)
+          case SPDef(vars, body) => SPDef(SPVar(variable name) +:  vars, body)
+          case default => SPDef(List(SPVar(variable.name)), newSP)
         }
         val newNewBound = newBound - variable
         if (newNewBound isEmpty) {
-          (SPAppl(SPDef(newFree.toArray.map(x => SPVar(x.name)) ++ func.vars, func.body), newFree.toArray.map(x => SPVar(x.name))), freeVariables, newNewBound)
+          (ApplyNArgs(SPDef(newFree.toList.map(x => SPVar(x.name)) ++ func.vars, func.body), newFree.toList.map(x => SPVar(x.name))), freeVariables, newNewBound)
         } else {
           (func, newNewBound, newFree)
         }
@@ -43,7 +54,7 @@ object SuperCombinator {
       case Appl(first, second) => {
         val (firstSP, firstBound, firstFree) = lambdaLifting(first, boundVariables, freeVariables, false)
         val (secondSP, secondBound, secondFree) = lambdaLifting(second, boundVariables, freeVariables, false)
-        (SPAppl(firstSP, Array(secondSP)), firstBound ++ secondBound, firstFree ++ secondFree)
+        (SPAppl(firstSP, secondSP), firstBound ++ secondBound, firstFree ++ secondFree)
       }
       case v: Var => {
         if (boundVariables contains v) {
@@ -55,4 +66,54 @@ object SuperCombinator {
       case b:BuiltIn => (b, boundVariables, freeVariables)
     }
   }
+  def ApplyNArgs(sp: SPTerm, vars: List[SPVar]): SPTerm = vars.headOption.fold(sp)(x => ApplyNArgs(SPAppl(sp, x), vars.tail))
+}
+
+object ApplN {
+  def unapply(term: SPTerm): Option[(Combinator, List[SPTerm])] = term match {
+    case SPAppl(term1, term2) => term1 match {
+      //case ApplN
+        case _: SPAppl => unapply(term1).map(x => (x._1,  x._2 :+ term2))
+        case c: Combinator => Some((c, List[SPTerm](term2)))
+        case _ => None
+      }
+    case _ => None
+  }
+}
+
+object SPReduce {
+  val combApply: SPTerm => Option[SPTerm] = {
+    case ApplN(applicable, paramsList) =>
+      applicable.result(paramsList)
+      //val result = applicable.result(paramsList)
+      //println(applicable +"(" + paramsList.mkString(", ") + ") => " + result)
+      //result
+    case default => {
+      None
+    }
+  }
+
+  def mu(reduction: SPTerm => Option[SPTerm]): SPTerm => Option[SPTerm] = {
+    case SPAppl(term, params) => reduction(term).map(SPAppl(_, params))
+    case _ => None
+  }
+
+  def nu(reduction: SPTerm => Option[SPTerm]): SPTerm => Option[SPTerm] = {
+    case SPAppl(term, param) => reduction(param).map(SPAppl(term, _))
+    case _ => None
+  }
+
+  def combMuNu(term: SPTerm ): Option[SPTerm] = {
+      //reduction(x).getOrElse((mu(reduction)(x).getOrElse(nu(reduction)(x))))
+      val l = List[SPTerm => Option[SPTerm]](combApply, mu(combMuNu), nu(combMuNu))
+      l.foldLeft[Option[SPTerm]]((None))((x, y) =>
+        x match {
+          case None => y(term)
+          case default => default
+        }
+      )
+    }
+
+  def toNormalForm(step: SPTerm => Option[SPTerm]): SPTerm => SPTerm =
+    (term: SPTerm) => step(term).fold(term)(toNormalForm(step))
 }
--- Term.scala
+++ Term.scala
@@ -17,23 +17,42 @@ case class Abstr(variable: Var, body: Term) extends Term {
 }
 
 trait BuiltIn extends Term with SPTerm
+trait Combinator extends SPTerm {
+  def result(x: List[SPTerm]): Option[SPTerm]
+}
 
 case class IntTerm(value: Int) extends BuiltIn {
   override def toString = value.toString
 }
 
-case object IntSum extends BuiltIn {
+case object IntSum extends BuiltIn with Combinator {
   override def toString = "+"
+  def result(x: List[SPTerm]): Option[SPTerm] = None
 }
 
-case object IntSub extends BuiltIn {
+case object IntSub extends BuiltIn with Combinator {
   override def toString = "-"
+  def result(x: List[SPTerm]): Option[SPTerm] = None
 }
 
-case object IntDiv extends BuiltIn {
+case object IntDiv extends BuiltIn with Combinator {
   override def toString = "div"
+  def result(x: List[SPTerm]): Option[SPTerm] = None
 }
 
-case object IntMult extends BuiltIn {
+case object IntMult extends BuiltIn with Combinator{
   override def toString = "*"
+  def result(x: List[SPTerm]): Option[SPTerm] = None
+}
+
+case object IntGtE extends BuiltIn with Combinator {
+  override def toString = ">="
+  def result(x: List[SPTerm]): Option[SPTerm] = {
+    val y = x.map({case i:IntTerm => Some(i); case _ => None}).filter(!_.isEmpty).map(_ get)
+    if (x.length == 2 && y.length == 2) {
+      Some(SPDef(List(SPVar("x"), SPVar("y")), if (y.head.value >= y.tail.head.value) SPVar("x") else SPVar("y")))
+    } else {
+      None
+    }
+  }
 }
